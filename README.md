07bb796c5ba5da586b11fa9a57d62939edda042c
Pilaton <83372686+Pilaton@users.noreply.github.com>
2021-12-15 03:38:21 +0300

    feat: Release!

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# OhMyZsh-full-autoupdate
- Automatic update of custom plugins and themes Oh My Zsh
+<div align="center">
+    <a href="https://github.com/Pilaton/OhMyZsh-full-autoupdate">
+        <img src="./doc/images/ohmyzsh-full-update.svg" alt="OhMyZsh Full-autoupdate" width="400">
+    </a>
+    <h1>🍃 OhMyZsh Full-autoupdate</h1>
+    <h4>Automatic update of custom plugins and themes <a href="https://github.com/ohmyzsh/ohmyzsh">Oh My Zsh</a></h4>
+</div>
+
+<br>
+
+## What is it
+
+As you probably know, when you update Oh My Zsh, your plugins and themes installed in the `~/.oh-my-zsh/custom` folder are not updated.  
+
+This plugin solves this problem and automatically updates your plugins and themes.
+
+## Installing
+
+1. Clone this repository into `$ZSH_CUSTOM/plugins` (`~/.oh-my-zsh/custom/plugins`)
+
+    ```bash
+    git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+    ```
+
+2. Add the plugin to the Oh My Zsh plugin list in the `~/.zshrc` file:
+
+    ```bash
+    plugins=(... ohmyzsh-full-autoupdate)
+    ```
+
+3. Start a new terminal session.
+
+## Usage
+
+The plugin **works completely automatically**.  
+Launches simultaneously with the Oh My Zsh update.  
+The update frequency depends on the Oh My Zsh settings, by default every 13 days.  
+
+💡 Note: It can be changed with the parameter `export UPDATE_ZSH_DAYS=13` in `~/.zshrc`.
+
+## License
+
+MIT © [Pilaton](https://github.com/Pilaton)
---
+++ ohmyzsh-full-autoupdate.plugin.zsh
@@ -0,0 +1,100 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#        File:  ohmyzsh-full-autoupdate.plugin.zsh
+#
+#        Name:  Oh My Zsh full-autoupdate
+# Description:  Plugin for Oh My ZSH that automatically updates your custom plugins and themes.
+#
+#      Author:  Pilaton
+#      GitHub:  https://github.com/Pilaton/MacSync
+#        Bugs:  https://github.com/Pilaton/MacSync/issues
+#     License:  MIT
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#######################################
+# If our label exists in the file "${ZSH_CACHE_DIR}/.zsh-update", skip updating plugins and themes
+#######################################
+if [[ ! -z $LABEL_FULL_AUTOUPDATE ]]; then
+    return 
+fi
+
+#######################################
+# Set colors if "tput" is present in the system
+#######################################
+if [[ ! -z $(which tput 2> /dev/null) ]]; then
+    bold=$(tput bold)
+    colorGreen=$(tput setaf 2)
+    colorYellow=$(tput setaf 3)
+    colorBlue=$(tput setaf 4)
+    reset=$(tput sgr0)
+fi
+
+#######################################
+# Welcome screen
+#######################################
+echo ""
+echo "${bold}Updating plugins and themes Oh My ZSH...${reset}"
+echo "${colorYellow}----------------------------------------${reset}"
+echo ""
+
+#######################################
+# Getting url for a package on the GitHub website.
+# Arguments:
+#   [text...] Path to the .git folder in the local package directory
+# Outputs:
+#   [text...] Url
+#######################################
+_getUrlGithub() {
+    local URL=$(grep 'url =' "${1}/config" | grep -o 'https://\S*' | sed 's/\.git//')
+    echo $URL
+}
+
+#######################################
+# Defining the plugin category and getting the category name.
+# Arguments:
+#   [text...] Path to local package folder
+# Outputs:
+#   [text...] Name category
+#######################################
+_getNameCustomCategory() {
+    case ${1} in
+        *"plugins"*) echo "Plugin" ; return 0 ;;
+        *"themes"*)  echo "Theme"  ; return 0 ;;
+    esac
+}
+
+#######################################
+# Saving a label that determines if plugins need to be updated.
+# Globals:
+#   ZSH_CACHE_DIR
+#######################################
+_savingLabel() {
+    echo "\nLABEL_FULL_AUTOUPDATE=true" >> "${ZSH_CACHE_DIR}/.zsh-update"
+    return 0
+}
+
+#######################################
+# We get a list of available plugins and update them.
+# Globals:
+#   ZSH_CUSTOM
+#######################################
+omzFullUpdate() {
+    local arrayPackages=( $(find -L "${ZSH_CUSTOM}" -type d -name ".git") )
+
+    for package in ${arrayPackages[@]}; do
+        local urlGithub=$(_getUrlGithub "$package")
+        local nameCustomCategory=$(_getNameCustomCategory "$package")
+        local packageDir=$(dirname "$package")
+        local packageName=$(basename "$packageDir")
+
+        echo "${colorYellow}Updating ${nameCustomCategory}${reset} — ${colorGreen}${packageName}${reset} -> ${colorBlue}($urlGithub)${reset}"
+        git -C "${packageDir}" pull
+        echo ""
+    done
+
+    # Start the function of saving the label
+    _savingLabel
+}
+omzFullUpdate
